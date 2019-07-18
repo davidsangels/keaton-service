bda9d5dac2cbb628ee5d7b57c4ff26dbaaa9d98d
Keaton Tatooles <keatontatooles@Keatons-MacBook-Air.local>
2019-07-17 19:39:54 -0700

    fixed database url

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://172.17.0.3/bookings';
+const mongoUri = 'mongodb://172.17.0.2/bookings';
 
 const db = mongoose.connect(mongoUri);
 
